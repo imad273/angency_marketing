4154b7fe7b2145b2912f748e73297f5d9cec89e8
imad273 <abbadimad0123@gmail.com>
2024-10-25 16:58:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# relvolta_marketing
---
+++ script.js
@@ -0,0 +1,19 @@
+/* Animation scroll initialize */
+AOS.init({
+  duration: 1200,
+});
+
+function openPhoneMenu() {
+  console.log("sxsx");
+  var x = document.getElementById("links");
+  x.style.display = "flex";
+  x.style.position = "fixed";
+}
+
+function closeMenu() {
+  var x = document.getElementById("links");
+  if (screen.width <= "767") {
+    x.style.display = "none";
+    x.style.position = "static";
+  }
+}
